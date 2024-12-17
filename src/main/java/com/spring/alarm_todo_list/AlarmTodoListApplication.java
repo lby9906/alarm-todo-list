b421ae9346681ff308ac9a5c318ecb94fac89153
lby9906 <lby9906@naver.com>
2024-12-17 17:44:05 +0900

    초기 설정

---
+++ src/main/java/com/spring/alarm_todo_list/AlarmTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.spring.alarm_todo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/alarm_todo_list/AlarmTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.alarm_todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
