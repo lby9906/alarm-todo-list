31a5ed682eaff7a9c741762aba4b073a04b0b3cc
lby9906 <lby9906@naver.com>
2024-12-19 23:43:12 +0900

    feat: Security 설정과 CustomException 처리

---
+++ src/main/java/com/spring/alarm_todo_list/exception/AlarmTodoListException.java
@@ -0,0 +1,10 @@
+package com.spring.alarm_todo_list.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AlarmTodoListException extends RuntimeException {
+    private ErrorCode errorCode;
+}
