45258946db2957f270b357a6d6caee0a4cd267c5
lby9906 <lby9906@naver.com>
2025-01-01 15:27:36 +0900

    feat: 회원 에러 코드 추가

--- src/main/java/com/spring/alarm_todo_list/exception/ErrorCode.java
+++ src/main/java/com/spring/alarm_todo_list/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    EXISTING_ALREADY_ACCOUNT(HttpStatus.BAD_REQUEST, "이미 가입된 회원입니다.");
+    EXISTING_ALREADY_ACCOUNT(HttpStatus.BAD_REQUEST, "이미 가입된 회원입니다."),
+    NOT_FOUNT_ACCOUNT(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
