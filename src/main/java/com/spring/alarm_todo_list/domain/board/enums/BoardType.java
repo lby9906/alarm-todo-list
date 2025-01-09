54aa1d9e0b265648caafb443b6678760039b8fad
lby9906 <lby9906@naver.com>
2025-01-09 14:16:04 +0900

    fix: 일정 등록 기능 코드 리뷰 반영

--- src/main/java/com/spring/alarm_todo_list/domain/board/enums/BoardType.java
+++ src/main/java/com/spring/alarm_todo_list/domain/board/enums/BoardType.java
@@ -1,8 +1,12 @@
 package com.spring.alarm_todo_list.domain.board.enums;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum BoardType {
-    TODO, DONE
+    TODO("할 일"), DONE("완료");
+
+    private final String content;
 }
